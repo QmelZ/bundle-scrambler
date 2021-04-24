236282514082d2b3e7020fb8014f5788efab8d74
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-04-24 11:13:11 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let original = Core.bundle.getProperties();
+let edit = Core.bundle.getProperties().copy();
+let keys = edit.keys().toSeq();
+
+edit.each((k, v) => {
+    let value = keys.random();
+    keys.remove(value);
+    edit.put(k, original.get(value));
+});
+
+Core.bundle.setProperties(edit);
+
+// the
+Events.on(ContentInitEvent, () => {
+    Vars.content.each(e => {
+        if(!(e instanceof UnlockableContent)) return;
+        let ee = e.getContentType() + "." + e.name;
+        e.localizedName = Core.bundle.get(ee + ".name");
+        e.description = Core.bundle.get(ee + ".description");
+        e.details = Core.bundle.get(ee + ".details");
+    });
+});
